17509950926d8ef5aa9c02c2f896ad7de5fee469
Karan Walekar <127884565+walekarkaran@users.noreply.github.com>
2026-09-01 22:29:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const el=document.querySelector(a.getAttribute('href'));if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth'});}}));
